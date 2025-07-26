1b4e6c3dc673b2e826ab5cb4704db6415baf7b47
i7 <bakasa.andrew@gmail.com>
2025-07-26 16:37:35 +0200

    Improved Media searching 5

--- app/media/metadata.ts
+++ app/media/metadata.ts
@@ -2,8 +2,24 @@
 import { Metadata } from 'next';
 
 export const defaultMetadata: Metadata = { // Good practice to have default metadata
-  title: 'Media',
-  description: 'Media Management',
+  title: 'EEG Media',
+  description: 'Projects View',
+     icons: {
+    icon: '/peeg.jpg', // This refers to app/icon.png
+    shortcut: '/shortcut-icon.png', // If you have a different shortcut icon
+    apple: '/peeg.jpg', // This refers to app/apple-icon.png
+    other: [
+      {
+        rel: 'apple-touch-icon-precomposed',
+        url: '/peeg.jpg',
+      },
+      {
+        rel: 'mask-icon',
+        url: '/peeg.jpg',
+        color: '#000000',
+      },
+    ],
+  },
 };
 
 // You can create a function to generate dynamic metadata:
